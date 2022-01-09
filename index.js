2549390d1f9b32e62a549e93132adfc57496072a
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-08 21:18:50 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+const findMatching = function(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === string.toLowerCase()
+  })
+}
+
+const fuzzyMatch = (drivers, index) => {
+  return drivers.filter(driver => driver.startsWith(index))
+}
+
+function matchName(drivers, name){
+  return drivers.filter(function(hometown){
+    return hometown.name === name
+  })
+}
